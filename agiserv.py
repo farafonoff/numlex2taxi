dab232b5e861cc5b14b57172a41decb89b6a8159
Artem Farafonov <artem_farafonov@epam.com>
2018-01-23 16:50:03 +0400

    agiserv WIP

--- agiserv.py
+++ agiserv.py
@@ -2,6 +2,7 @@ import random
 import itertools
 from pprint import pprint
 import asyncio
+from asyncio_extras import threadpool
 from panoramisk import fast_agi
 
 import known_operators
@@ -25,29 +26,28 @@ print(get_devices_for_number('+79090561017'))
 
 loop = asyncio.get_event_loop()
 
-STATUS_CONTINUE = ['CONGESTION', 'CHANUNAVAIL']
+@asyncio.coroutine
+def dial_status(request):
+    resp = (yield from request.send_command('GET VARIABLE DIALSTATUS'))
+    return resp['result'][1]
+
+STATUS_CONTINUE = ['INIT', 'CONGESTION', 'CHANUNAVAIL']
 
 @asyncio.coroutine
-def dump_status(request):
-    pprint(['DIALSTATUS', (yield from request.send_command('GET VARIABLE DIALSTATUS'))])
-    pprint(['HANGUPCAUSE', (yield from request.send_command('GET VARIABLE HANGUPCAUSE'))])
-
-@asyncio.coroutine
-def call_waiting(request):
-    pprint(['AGI variables:', request.headers])
-    pprint((yield from request.send_command('EXEC Dial Local/123@test_busy')))
-    yield from dump_status(request)
-    pprint((yield from request.send_command('EXEC Dial Local/123@test_congestion')))
-    yield from dump_status(request)
-    pprint((yield from request.send_command('EXEC Dial Local/123@test_answer')))
-    yield from dump_status(request)
-    pprint((yield from request.send_command('EXEC Dial Local/123@test_fail')))
-    yield from dump_status(request)
+def dialplan(request):
+    number = request.headers['agi_extension']
+    dialstatus = 'INIT'
+    async with threadpool():
+        peers = get_devices_for_number(number)
+    pprint(peers)
+    while dialstatus in STATUS_CONTINUE:
+        yield from request.send_command('EXEC Dial Local/123@test_busy')
 
 def main():
     fa_app = fast_agi.Application(loop=loop)
     fa_app.add_route('call_waiting', call_waiting)
     fa_app.add_route('test', call_waiting)
+    fa_app.add_route('dial', dialplan)
     coro = asyncio.start_server(fa_app.handler, '0.0.0.0', 4574, loop=loop)
     server = loop.run_until_complete(coro)
 
